9cb1751019f574e0db132bffa7d5332c0af105fc
Lodhi-Haris <130280684+Lodhi-Haris@users.noreply.github.com>
2023-05-24 02:54:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var inp = document.getElementById("haris")
+var inp1 = document.getElementById("lodhi")
+var inp2 = document.getElementById("khan")
+var inp3 = document.getElementById("sultan") 
+var rad = document.querySelector("input[type = radio]:checked");
+// var val = rad.value; 
+ 
+
+
+
+function register(){
+    console.log(inp.value);
+    console.log(inp1.value);
+    console.log(inp2.value);
+    console.log(inp3.value);
+    console.log(rad.value);
+
+}
